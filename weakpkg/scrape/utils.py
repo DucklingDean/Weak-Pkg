6cb7d25996affcab0837d7a691103a89be151a7c
DucklingDean <duckling.dean@proton.me>
2024-10-30 11:10:57 +0000

    [FEAT] Add scraper for wordlist page
    |Extract Addtional info:
      - cracking rate.
      - download file name.
      - torrent file name.
      - hashsum.
      - compressed size.

--- weakpkg/scrape/utils.py
+++ weakpkg/scrape/utils.py
@@ -32,3 +32,28 @@ def wordlists_in_page(html:str) -> list[dict]:
         })
 
     return all_wordlists
+
+
+
+def wordlist_info(html:str) -> dict[str,str]:
+    tree          = HTMLParser(html)
+    download_btns = tree.css("div.column.is-6.mb-3")
+    hashsum_elmnt = tree.css(".table.is-fullwidth tr")[:2]
+    hashes        = ''
+    
+    # extract hashes
+    for tr in hashsum_elmnt:
+        hashes += ":".join(td.text().strip() for td in tr.css("td")) # name:md5:hash
+        hashes += "|||"  # append "|||".
+    hashes.rstrip("|||") # remove the last "|||".
+
+
+
+
+    return {
+        "rate"     :tree.css(".mt-4.is-size-3.has-text-weight-bold")[1].text().strip(), # crack rate.
+        "comp_size":tree.css(".mb-4.is-size-3.has-text-weight-bold")[3].text().strip(), # compressed size.
+        "dw_name"  :download_btns[0].text().strip(), # download file name.
+        "tr_name"  :download_btns[1].text().strip(), # torrent file name.
+        "hashsum"  :hashes,                         
+    }
