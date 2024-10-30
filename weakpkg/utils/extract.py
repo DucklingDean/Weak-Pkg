9d1c6b0f06ef49b111f7c04d9ea9485ba68f2478
DucklingDean <duckling.dean@proton.me>
2024-10-30 00:07:50 +0000

    [FEAT] Extrat all wordlists info & loop through the whole website
    - better printing

--- weakpkg/utils/extract.py
+++ weakpkg/utils/extract.py
@@ -1,7 +1,8 @@
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
-from scrape import lastpage
-from printool import waiting_animation, print_incolumn, clear_line, Color
+from selenium.webdriver.support.ui import Select
+from scrape import lastpage, wordlists_in_page
+from printool import *
 from time   import sleep
 
 
@@ -20,24 +21,21 @@ DV_OPTIONS.add_argument("--disable-dev-shm-usage")
 
 def p(txt:str) -> None:
     clear_line()
-    print(f"\r{txt}", end="")
+    print(f"\r[*] {txt}", end="")
 
 
 
-
-def wait(dv:Firefox, start:int) -> None:
-    animation = waiting_animation()
-    while dv.execute_script("return document.readyState;") != "complete":
-        print_incolumn(start, next(animation))
-        sleep(0.2)
-
-    clear_line()
+   
 
 
+def setup_env() -> tuple[Firefox, int]:
+    def wait(dv:Firefox, start:int) -> None:
+        animation = waiting_animation()
+        while dv.execute_script("return document.readyState;") != "complete":
+            print_incolumn(2, next(animation))
+            sleep(0.1)
+        clear_line()
 
-
-
-def get_totalpages() -> Firefox:
     TERMINAL_COLOR.yellow
 
     p("Lunching Browser...")
@@ -49,7 +47,7 @@ def get_totalpages() -> Firefox:
 
 
     p("Waiting For Response...")
-    wait(dv, 24)
+    wait(dv, 28)
     TERMINAL_COLOR.white
     
 
@@ -62,22 +60,69 @@ def get_totalpages() -> Firefox:
         f" {max_pages}", sep=""
         
     )
-    return dv
+    return dv, max_pages
+
+
+
+
+def get_allpages_html(dv:Firefox, max_pages:int) -> list[str]:
+    print(
+        C.yellow, C.bold,
+        "[!] Fetching Page:",
+        C.reset, " -",
+        sep=""
+    )
+    print(
+        C.bright_green, C.bold,
+        "[+] HTML         :", 
+        C.reset, " -",
+        sep="" 
+    )
+    line_up()
+    html      = dv.page_source
+    wordlists = []
+    animation = waiting_animation()
+    for page in range(2, max_pages+1):
+        dv.get(f"https://weakpass.com/wordlists?page={page}")
+
+        print_incolumn(20, f"{page-1}/{max_pages}")
+        wordlists += wordlists_in_page(html)
+ 
+        line_up()
+        print_incolumn(20, f"{page}/{max_pages}")
+
+
+        while dv.execute_script("return document.readyState;") != "complete":
+            print_incolumn(2, next(animation))
+            sleep(0.1)
+
+        line_down()
+        html = dv.page_source
+        if page == max_pages:
+            wordlists += wordlists_in_page(html)
+            print_incolumn(20, f"{max_pages}/{max_pages}")
+            print_incolumn(2, "\u2714")
+            line_up()
+            print_incolumn(2, "\u2714")
+            line_down()
+            print()
+
+    return wordlists
 
 
 
 
 def extract_all():
-    dv = get_totalpages()
-
-
-
-
-
+    dv, max_pages = setup_env()
     
-    
+    wordlists = get_allpages_html(dv, max_pages)
+    clear_line()
+    print(len(wordlists))
     dv.close()
     
+
+
+
 
 if __name__=="__main__":
     extract_all()
